e368f1723d387e5fd0c173c743eb268a40e8d8f5
William ESCANDE <wlliam.escande@gmail.com>
2016-08-10 13:08:11 +0200

    J06 update

--- J06/KOmpilaThorJ06.sh
+++ J06/KOmpilaThorJ06.sh
@@ -17,19 +17,81 @@ echo "alias KOmpilaThorJ06='sh $0'"
 else
 EX=$(echo "$1" | sed 's/.*\/ex/ex/' | cut -c 1-4)
 
+DOSSIER=$(echo "$1" | sed "s/$EX.*/$EX/")
+
+PUTCHAR=$(cat "$1" | grep 'ft_putchar' | head -n 1 | cut -c 1)
+
+if [ "$PUTCHAR" = "v" ]
+then
+echo '/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_KOmpilaThor.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wescande <wescande@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/08 12:10:30 by wescande          #+#    #+#             */
+/*   Updated: 2016/08/08 12:10:32 by wescande         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+' > ft_putchar_KOmpilaThor.c
+else
+echo '/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_KOmpilaThor.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wescande <wescande@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/08 12:10:30 by wescande          #+#    #+#             */
+/*   Updated: 2016/08/08 12:10:32 by wescande         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+' > ft_putchar_KOmpilaThor.c
+fi
+
+
 if [ "$EX" = "ex00" ]
 then
+	echo "Exercice 00 Affichage des fichiers dans $DOSSIER"
+	find $DOSSIER | sed "s/$EX//"
+	echo "Lancement du script"
+	sh $DOSSIER/libft_creator.sh
+	echo "Affichage des fichiers dans $DOSSIER"
+	find $DOSSIER | sed "s/$EX//"
+fi
+
+if [ "$EX" = "ex01" ]
+then
+	echo "Exercice 01 dans $DOSSIER"
+	echo "Compilation :"
+	gcc -Wall -Wextra -Werror ft_print_program_name.c ft_putchar_KOmpilaThor.c -o ./.KOmpilaThorJ06.compile
+	echo "Affichage du nom du programme (./.KOmpilaThorJ06.compile)"
+	./.KO
 
 fi
 
 
-gcc -Wall -Wextra -Werror J06.c ft_putchar_KOmpilaThor.c $1 -o ./.KOmpilaThorJ06.compile
+# ./.KOmpilaThorJ06.compile
 
-./.KOmpilaThorJ06.compile
+ rm ./.KOmpilaThorJ06.compile ft_putchar_KOmpilaThor
 
-rm ./.KOmpilaThorJ06.compile ft_putchar_KOmpilaThor
-
-norminette -R CheckForbiddenSourceHeader $1
+norminette -R CheckForbiddenSourceHeader $DOSSIER
 
 
 fi
