859a0d4ee0798a66d24fc4eadb5b455bed463749
william <william.escande@gmail.com>
2016-09-28 01:05:49 +0200

    jusqu'à strclr

--- LIBFT/KOmpilaThorLIBFT.sh
+++ LIBFT/KOmpilaThorLIBFT.sh
@@ -43,6 +43,7 @@ echo '/* ***********************************************************************
 #define GRE 32
 #define BLU 36
 #define WHI 37
+#define YEL 33
 
 
 void	print_function(char *name, int correct)
@@ -54,11 +55,21 @@ void	print_function(char *name, int correct)
 	}
 	else
 	{
-		color(RED);
 		if (correct < 0)
+		{
+			color(RED);
 			printf("Function: %s is missing from the library\\n", name);
-		color(RED);
-		printf("<====================FAIL ! ! ! !======================>\\n");
+		}
+		else if (correct == 50)
+		{
+			color(YEL);
+			printf("<=================YOU HAVE TO CHECK !==================>\\n");
+		}
+		else
+		{
+			color(RED);
+			printf("<====================FAIL ! ! ! !======================>\\n");
+		}
 	}
 	color(0);
 	printf("\\n");
@@ -74,9 +85,12 @@ int		main(void)
 	int		i_ans;
 	void	*v_ret_tmp;
 	void	*v_ans_tmp;
+	char	*s_ret;
 	int		correct;
 	int		i;
+	int		j;
 	(void) i;
+	(void) j;
 	(void) v_ret;
 	(void) v_ans;
 	(void) v_ret_tmp;
@@ -1240,6 +1254,136 @@ else
 	print_function("ft_'$EXO'", -1);
 		' >> KOMP_LIBFT.c
 fi
+
+EXO="memalloc"
+nm libft.a | grep ":" | grep -q ft_$EXO
+if [ $? -eq 0 ]
+then
+echo '
+	strcpy(nom, "ft_'$EXO'");
+	printf("Ex: %s\\n", nom);
+	print_function(nom, 50);
+' >> KOMP_LIBFT.c
+else
+	echo '
+	print_function("ft_'$EXO'", -1);
+		' >> KOMP_LIBFT.c
+fi
+
+EXO="memdel"
+nm libft.a | grep ":" | grep -q ft_$EXO
+if [ $? -eq 0 ]
+then
+echo '
+	strcpy(nom, "ft_'$EXO'");
+	printf("Ex: %s\\n", nom);
+	print_function(nom, 50);
+' >> KOMP_LIBFT.c
+else
+	echo '
+	print_function("ft_'$EXO'", -1);
+		' >> KOMP_LIBFT.c
+fi
+
+EXO="strnew"
+nm libft.a | grep ":" | grep -q ft_$EXO
+if [ $? -eq 0 ]
+then
+echo '
+	strcpy(nom, "ft_'$EXO'");
+	printf("Ex: %s\\n", nom);
+	size_t	val_send_'$EXO'[5] = {5, 4, 0, 10, 30};
+	i = 0;
+	correct = 0;
+	while (i < 5)
+	{
+		s_ret = ft_'$EXO'(val_send_'$EXO'[i]);
+		j = 0;
+		while (j < (int)val_send_'$EXO'[i])
+		{
+			if (*s_ret)
+			{
+				printf("Test #%d fait avec les valeurs : ", i);
+				color(BLU);
+				printf("|%lu|\\n", val_send_'$EXO'[i]);
+				color(0);
+				correct++;
+				break;
+			}
+			++s_ret;
+			++j;
+		}
+		++i;
+	}
+	print_function(nom, correct);
+' >> KOMP_LIBFT.c
+else
+	echo '
+	print_function("ft_'$EXO'", -1);
+		' >> KOMP_LIBFT.c
+fi
+
+EXO="strdel"
+nm libft.a | grep ":" | grep -q ft_$EXO
+if [ $? -eq 0 ]
+then
+echo '
+	strcpy(nom, "ft_'$EXO'");
+	printf("Ex: %s\\n", nom);
+	print_function(nom, 50);
+' >> KOMP_LIBFT.c
+else
+	echo '
+	print_function("ft_'$EXO'", -1);
+		' >> KOMP_LIBFT.c
+fi
+
+EXO="strclr"
+nm libft.a | grep ":" | grep -q ft_$EXO
+if [ $? -eq 0 ]
+then
+echo '
+	strcpy(nom, "ft_'$EXO'");
+	printf("Ex: %s\\n", nom);
+	char	val_send_'$EXO'[5][50] = {"", "sv sdqsd fds f", "\200", "WTF", "p"};
+	i = 0;
+	correct = 0;
+	while (i < 5)
+	{
+		j = strlen(val_send_'$EXO'[i]);
+		ft_'$EXO'(val_send_'$EXO'[i]);
+		memcpy(s_ret, val_send_'$EXO'[i], j);
+		while (j)
+		{
+			if (*s_ret)
+			{
+				printf("Test #%d fait avec les valeurs : ", i);
+				color(BLU);
+				printf("|%s|\\n", val_send_'$EXO'[i]);
+				color(0);
+				correct++;
+				break;
+			}
+			++s_ret;
+			--j;
+		}
+		++i;
+	}
+	print_function(nom, correct);
+' >> KOMP_LIBFT.c
+else
+	echo '
+	print_function("ft_'$EXO'", -1);
+		' >> KOMP_LIBFT.c
+fi
+
+
+
+
+
+
+
+
 
 echo '
 	printf("\\n");
